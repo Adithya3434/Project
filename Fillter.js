e9cca3f02e7b4ab7b514b30628268171f91322af
Adithya S Nair <adithyasnair340@gmail.com>
2024-11-04 09:21:41 +0530

    second commit

--- Fillter.js
+++ Fillter.js
@@ -30,24 +30,25 @@
 // console.log(d);
 
 // 3q 
-
 // let a=[1, 2, 2, 3, 4, 4, 5];
 // let b=[];
 // a.filter(i=>{
-//     if(b[i]){
-
+//     if(b.includes(i)){
+//         return false
 //     }
 //     else{
 //         b.push(i)
-//     }
-        
+//         return true
+//     }       
 // })
 // console.log(b);
 
+//4q
 // let a=[0, "hello", false, "", undefined, 42];
 // let b=a.filter(Boolean)
 // console.log(b);
 
+//5q
 // let a=[3, 12, 19, 21, 25, 30];
 // let b=[];
 // a.filter(i=>{
@@ -57,13 +58,18 @@
 // })
 // console.log(b);
 
-// let a=[1, 2, 3, 4];
+// 6q 
+// let a=[1, 2, 3,4];
+// product(a);
+// function product(a){
 // let b=a.reduce((c,i)=>{
 //     c=c*i;
 //     return c;
 // })
 // console.log(b);
+// }
 
+// 7q  
 // let a= ["apple", "banana", "apple", "orange", "banana", "apple"];
 // let b=a.reduce((c,i)=>{
 //     if(c[i]){
@@ -77,6 +83,20 @@
 // console.log(b);
 
 // 8q
+// arr=[1,[2,3],[4,[5,6]]];
+// console.log(flatten(arr));
+// function flatten(a){
+// return a.reduce((c,i)=>{
+//         if(Array.isArray(i)){
+//            return c.concat(flatten(i))
+//         }
+//         else{
+//            return c.concat(i)
+//         } 
+//     },[])    
+// }
+
+
 
 //9q
 // let people = [
@@ -93,3 +113,31 @@
 //     return a
 //   },{})
 // console.log(c);
+
+
+
+// 10q 
+// arr=[['name', 'Alice'], ['age', 25], ['job', 'Engineer']];
+// function getKeys(arr){
+//     return arr.reduce((b1,c)=>{
+//         b1[c[0]]=c[1];
+//         return b1
+//     },[])
+// }
+// console.log(getKeys(arr));
+
+
+
+// arr([1, 2, 2, 3, 4, 4, 5]);
+// let a1=[];
+// function arr(a) {
+//     a.filter(v=>{
+//         if(a1[v]) {                   
+//         }
+//         else {
+//             a1.push(v)
+//         }
+        
+//     });
+// }
+// console.log(a1);
